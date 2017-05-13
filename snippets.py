a685c48ec4934bffb69903d1f2e8fc8d695ec9f4
jsudufour <jsudufour@gmail.com>
2017-05-13 13:04:21 -0400

    Added stub for get function.

---
+++ snippets.py
@@ -0,0 +1,12 @@
+import logging
+
+#Set the log output file, and the log level
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name, snippet):
+    """
+    Stores a snippet with an associated name.
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - get{!r}".format(name))
+    return ""
