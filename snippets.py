3ecbaf0e9524f00af14357df580c2925a4ca95e9
jsudufour <jsudufour@gmail.com>
2017-05-13 13:13:07 -0400

    Fixed the put stub and added a get stub.

--- snippets.py
+++ snippets.py
@@ -8,5 +8,16 @@ def put(name, snippet):
     Stores a snippet with an associated name.
     Returns the name and the snippet
     """
-    logging.error("FIXME: Unimplemented - get{!r}".format(name))
+    logging.error("FIXME: Unimplemented - put{!r}, {!r}".format(name, snippet))
+    #Think about error handling here in case get fails - raise exception with message
+    return ""
+
+def get(name):
+    """
+    Retrieve the snippet with the given name.
+    If there is no such snippet, return '404 Snippet Not Found'.
+    Returns the snippet.
+    """
+    logging.error("FIXME - Unimplemented - get{!r}".format(name))
+    #Think about error handling here in case it fails - raise exception with message
     return ""
